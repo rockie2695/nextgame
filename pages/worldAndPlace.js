9afe584c1c9c4c458e923301f042aeb67cc50063
rockie2695 <21993381+rockie2695@users.noreply.github.com>
2021-06-07 23:11:13 +0800

    add worldResult

--- pages/worldAndPlace.js
+++ pages/worldAndPlace.js
@@ -25,14 +25,18 @@ export default function worldAndPlace() {
     }
   }, [router]);
   const { selectDC, selectNDC } = router.query;
-  const { loading: useDCWorldLoading, data: dCdata } = useWorld(
+  const useDCWorldResult = useWorld(
     `email=${session.user.email}&directControl=true`,
     selectDC === "true"
   );
-  const { loading: useNDCWorldLoading, data: nDCdata } = useWorld(
+  const useNDCWorldResult = useWorld(
     `email=${session.user.email}&directControl=false`,
     selectNDC === "true"
   );
+  const worldResult = [
+    { ...useDCWorldResult, enable: selectDC },
+    { ...useNDCWorldResult, enable: selectNDC },
+  ]; //return {loading,data}
   const tabArray = [
     {
       headerName: "Direct Control World",
@@ -75,56 +79,77 @@ export default function worldAndPlace() {
             </ActiveLink>
           ))}
         </div>
-        <section className={styles.section_container}>
-          <header>
-            <span className={styles.mainHeader}>Direct Control</span>
-            <p>direct control would get all money</p>
-          </header>
-          {useDCWorldLoading ? (
-            <section className={styles.world}>
-              <header>
-                <span className={styles.subHeader}>
-                  <Skeleton style={{ width: "50%" }} />
-                </span>
-              </header>
-              <p>
-                <Skeleton />
-              </p>
-              <div>
-                {[...Array(12)].map((place, index) => (
-                  <Skeleton
-                    key={index}
-                    style={{
-                      width: "6rem",
-                      height: "6rem",
-                      border: "1px solid black",
-                      marginRight: "0.5rem",
-                      marginBottom: "0.5rem",
-                      borderRadius: "0.5rem",
-                    }}
-                  />
-                ))}
-              </div>
-            </section>
-          ) : null}
-          {dCdata &&
-            dCdata.success &&
-            dCdata.data.map((world) => (
-              <section key={world._id} className={styles.world}>
-                <header>
-                  <span className={styles.subHeader}>{world.name} 世界</span>
-                  <button>rename</button>
-                </header>
-                <p>
-                  [{world.type}世界]
-                  {world.type === "蒸汽"
-                    ? "第一次工業革命,使用蒸汽機、煤、鐵和鋼進行工業化"
-                    : null}
-                </p>
+        <section>
+          {worldResult.map((result, index) =>
+            result.enable ? (
+              <section className={styles.section_container} key={index}>
+                {index === 0 ? (
+                  <header>
+                    <span className={styles.mainHeader}>Direct Control</span>
+                    <p>direct control would get all money</p>
+                  </header>
+                ) : (
+                  <header>
+                    <span className={styles.mainHeader}>
+                      Non Direct Control
+                    </span>
+                    <p>non direct control would get X0.1 money</p>
+                  </header>
+                )}
 
-                <MultiPlace worldId={world._id} email={session.user.email} />
+                {result.loading ? (
+                  <section className={styles.world}>
+                    <header>
+                      <span className={styles.subHeader}>
+                        <Skeleton style={{ width: "50%" }} />
+                      </span>
+                    </header>
+                    <p>
+                      <Skeleton />
+                    </p>
+                    <div>
+                      {[...Array(12)].map((place, index) => (
+                        <Skeleton
+                          key={index}
+                          style={{
+                            width: "6rem",
+                            height: "6rem",
+                            border: "1px solid black",
+                            marginRight: "0.5rem",
+                            marginBottom: "0.5rem",
+                            borderRadius: "0.5rem",
+                          }}
+                        />
+                      ))}
+                    </div>
+                  </section>
+                ) : null}
+                {result.data &&
+                  result.data.success &&
+                  result.data.data.map((world) => (
+                    <section key={world._id} className={styles.world}>
+                      <header>
+                        <span className={styles.subHeader}>
+                          {world.name} 世界
+                        </span>
+                        <button>rename</button>
+                      </header>
+                      <p>
+                        [{world.type}世界]
+                        {world.type === "蒸汽"
+                          ? "第一次工業革命,使用蒸汽機、煤、鐵和鋼進行工業化"
+                          : null}
+                      </p>
+
+                      <MultiPlace
+                        worldId={world._id}
+                        email={session.user.email}
+                      />
+                    </section>
+                  ))}
               </section>
-            ))}
+            ) : null
+          )}
         </section>
         <aside>
           <nav>
