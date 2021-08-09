3aa23222b77719125b5280ed0746e0674b4db39d
rockie2695 <21993381+rockie2695@users.noreply.github.com>
2021-08-09 23:13:13 +0800

    incomplete

--- pages/room.js
+++ pages/room.js
@@ -3,16 +3,16 @@ import Layout, { siteTitle } from "../components/Layout";
 import { useSession, getSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import { useEffect, useState, useRef } from "react";
-import webSocket from "socket.io-client";
+import socket from "../components/socket";
 import { isNull } from "lodash";
 import mainStyles from "../styles/main.module.css";
 export default function room() {
   const [session, loading] = useSession();
   const router = useRouter();
-  const [ws, setWs] = useState(null);
+  //const [ws, setWs] = useState(null);
   if (typeof window !== "undefined" && loading) return null;
-  const wsRef = useRef(ws);
-  wsRef.current = ws;
+  //const wsRef = useRef(ws);
+  //wsRef.current = ws;
   useEffect(() => {
     if (router) {
       if (!session) {
@@ -25,29 +25,20 @@ export default function room() {
   }, [router]);
   useEffect(() => {
     console.log("startConnectWS");
-    connectWebSocket();
+    socket.connect();
     return () => {
-      if (!isNull(wsRef.current)) {
-        wsRef.current.close();
-      }
-      setWs(() => null);
+      socket.close();
     };
   }, []);
-  const initWebSocket = () => {
-    // Server 通知完後再傳送 disConnection 通知關閉連線
-    ws.on("disConnection", () => {
-      ws.close();
+  socket.onAny((event, ...args) => {
+    console.log(event, args);
+  });
+  const testAddRoom = () => {
+    socket.emit("addRoom", {
+      roomName: "testAddRoom",
+      creator: session.user.name || session.user.email,
     });
   };
-  const connectWebSocket = () => {
-    //開啟
-    setWs(webSocket("http://localhost:3001/"));
-  };
-  useEffect(() => {
-    if (ws) {
-      initWebSocket();
-    }
-  }, [ws]);
   return (
     <Layout>
       <Head>
@@ -56,7 +47,9 @@ export default function room() {
       <header>
         <span className={mainStyles.mainHeader}>Room</span>
       </header>
-      <div></div>
+      <div>
+        <button onClick={testAddRoom}>test add Room</button>
+      </div>
     </Layout>
   );
 }
